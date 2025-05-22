a4410d9e57649c103eef50cfab54834f0d3eff3c
aldaydev <rafaldayparejo@gmail.com>
2025-05-22 14:55:32 +0200

    working on routes

--- src/router.ts
+++ src/router.ts
@@ -2,16 +2,9 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hola con TypeSript')
-});
-
-router.get('/nosotros', (req, res) => {
-    res.send('Nosotros')
-});
-
-router.get('/blog', (req, res) => {
-    res.send('Blog')
-});
+/** Autenticación y registro */
+router.post('/auth/register', (req, res) => {
+    console.log('Desde register');
+})
 
 export default router;
