1e0840ae987d41297d3291c282c1910210df0d0b
aldaydev <rafaldayparejo@gmail.com>
2025-05-23 11:36:21 +0200

    working on error response

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,8 +2,17 @@ import type { Request, Response } from "express";
 import User from "../models/User";
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    const { email } = req.body;
+
+    const userExists = await User.findOne({email});
+    if(userExists){
+        const error = new Error('El usuario ya está registrado');
+        res.status(400).json({error: error.message});
+    }
+
     const user = new User(req.body);
     await user.save(req.body);
 
-    res.send('Registro creado correctamente');
+    res.status(201).send('Registro creado correctamente');
 }
