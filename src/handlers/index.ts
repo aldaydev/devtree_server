9f1b4ba33b3db27a2693c94eedf784bf3f479f65
aldaydev <rafaldayparejo@gmail.com>
2025-05-25 00:09:01 +0200

    working on validations

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,36 +1,44 @@
 import type { Request, Response } from "express";
+import { validationResult } from 'express-validator'
 import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
-    
-        const { email, password } = req.body;
 
+    try {
+        // Manejar errores
+        let errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            res.status(400).json(errors.array());
+            return
+            // throw new Error(errors.array());
+        }
+        
+        console.log('Esto se ejecuta');
+        const { email, password } = req.body;
         //Validar si el usuario existe
         const userExists = await User.findOne({email});
         if(userExists){
             throw new Error('El usuario ya está registrado');
         }
-
         //Validar si existe el handle
         const handle = slug(req.body.handle, '');
         const handleExists = await User.findOne({handle});
         if(handleExists){
             throw new Error('Nombre de usuario no disponible');
         }
-
         //Instanciar nuevo usuario referente al schema User
         const user = new User(req.body);
-
         //Hash del password y actualización en user
         const hash = await hashPassword(password);
         user.password = hash;
         user.handle = handle;
-
         //Guardar usuario en MongoDB
         await user.save();
-
         res.status(201).send('Registro creado correctamente');
-    
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { body } from 'express-validator';
 import { createAccount } from "./handlers";
 
 const router = Router();
 
 /** Autenticación y registro */
-router.post('/auth/register', createAccount);
+router.post('/auth/register', 
+    body('handle').notEmpty().withMessage('El handler no puede estar vacío'),
+    body('name').notEmpty().withMessage('El nombre no puede estar vacío'),
+    body('email').isEmail().withMessage('Email no válido'),
+    body('password').isLength({min: 8}).withMessage('Contraseña demasiado corta. Mínimo 8 caracteres.'),
+    createAccount
+);
 
 export default router;
